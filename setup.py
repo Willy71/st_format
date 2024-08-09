b1dc2e8cac7c4166fb1a827f82e63411d216aca4
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2024-08-09 13:44:51 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from setuptools import setup, find_packages
 
+with open("readme.md", "r") as arq:
+    readme = arq.read()
+
 setup(
     name='st_format',
-    version='0.1',
-    description='Utility functions for enhancing Streamlit apps',
+    version='0.2',
+    license='MIT License',
     author='Guillermo Cerato',
+    long_description=readme,
+    long_description_content_type='text/markdown',
     author_email='gcerato@gmail.com',
-    url='https://github.com/willy71/st_format',
-    packages=find_packages(),
-    install_requires=[
-        'streamlit',
-    ],
+    keywords='utilitys streamlit center text background',
+    description='Utility functions for enhancing Streamlit apps',
+    url='https://github.com/Willy71/st_format',
+    packages=['st_format'],
+    install_requires=['streamlit'],
 )
